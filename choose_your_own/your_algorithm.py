505923a557f4bf5fbba075ecf726c5eff0b2d014
alvsanand <alvsanand@gmail.com>
2015-08-11 21:22:58 +0200

    Uploaded soltion for miniproject 5

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from matplotlib.pyplot import clf
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -31,12 +32,93 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn import neighbors
+from sklearn.metrics import accuracy_score
+ 
+best_score = 0
+best_clf = None
+best_weights = None
+best_n_neighbors = None
+ 
+scores = []
+for weights in ['uniform', 'distance']:
+    for n_neighbors in [2,5,10,20,50,100]:
+        clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
+        clf.fit(features_train, labels_train)
+         
+        pred = clf.predict(features_test)
+ 
+        score = accuracy_score(labels_test, pred)
+         
+        scores.append(score)
+ 
+        if(score>best_score):
+            best_score = score
+            best_clf = clf
+            best_weights = weights
+            best_n_neighbors = n_neighbors
+ 
+print "Scores: %s"%(str(scores))
+print "The best NearestNeighbors[weights=%s, n_neighbors=%d]: %f"%(best_weights, best_n_neighbors, best_score)
 
 
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.metrics import accuracy_score
+#  
+# best_score = 0
+# best_clf = None
+# best_n_estimators = None
+#  
+# scores = []
+#  
+# for n_estimators in [2,5,10,20,50,100]:
+#     clf = RandomForestClassifier(n_estimators=n_estimators, max_depth=None, min_samples_split=1, random_state=0)
+#     clf.fit(features_train, labels_train)
+#      
+#     pred = clf.predict(features_test)
+#  
+#     score = accuracy_score(labels_test, pred)
+#      
+#     scores.append(score)
+#  
+#     if(score>best_score):
+#         best_score = score
+#         best_clf = clf
+#         best_n_estimators = n_estimators
+#  
+# print "Scores: %s"%(str(scores))
+# print "The best RandomForestClassifier[n_estimators=%d]: %f"%(best_n_estimators, best_score)
 
 
+# from sklearn.ensemble import AdaBoostClassifier
+# from sklearn.metrics import accuracy_score
+# 
+# best_score = 0
+# best_clf = None
+# best_n_estimators = None
+# 
+# scores = []
+# 
+# for n_estimators in [10,25,50,100,150,250]:
+#     clf = AdaBoostClassifier(n_estimators=n_estimators)
+#     clf.fit(features_train, labels_train)
+#     
+#     pred = clf.predict(features_test)
+# 
+#     score = accuracy_score(labels_test, pred)
+#     
+#     scores.append(score)
+# 
+#     if(score>best_score):
+#         best_score = score
+#         best_clf = clf
+#         best_n_estimators = n_estimators
+# 
+# print "Scores: %s"%(str(scores))
+# print "The best AdaBoostClassifier[n_estimators=%d]: %f"%(best_n_estimators, best_score)
 
 
+clf = best_clf
 
 try:
     prettyPicture(clf, features_test, labels_test)
--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,30 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print len(enron_data)
+print len(enron_data["SKILLING JEFFREY K"])
+print len(filter(lambda (k,v): v["poi"], enron_data.items()))
 
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+print enron_data["COLWELL WESLEY"]['from_this_person_to_poi']
+print enron_data["SKILLING JEFFREY K"]['exercised_stock_options']
+
+total_payments = []
+for k in enron_data.keys():
+    if 'total_payments' in enron_data[k] and enron_data[k]['total_payments']!='NaN' and k!= 'TOTAL':
+        total_payments.append((enron_data[k]['total_payments'], k))
+        
+total_payments = sorted(total_payments)    
+
+print "%s: %s" %(total_payments[len(total_payments)-1][1], total_payments[len(total_payments)-1][0])
+
+print "salary: %d" %(len(filter(lambda x: 'salary' in x[1] and x[1]['salary']!='NaN', enron_data.items())))
+print "email_address: %d" %(len(filter(lambda x: 'email_address' in x[1] and x[1]['email_address']!='NaN', enron_data.items())))
+print "%%total_payments: %f" %(len(filter(lambda x: 'total_payments' in x[1] and x[1]['total_payments']=='NaN', enron_data.items()))/float(len(enron_data)))
+print "%%poi total_payments: %f" %(len(filter(lambda x: 'total_payments' in x[1] and x[1]['total_payments']=='NaN' and x[1]['poi'], enron_data.items()))/float(len(filter(lambda (k,v): v["poi"], enron_data.items()))))
+print "#number + 10: %d" %(10+len(enron_data))
+print "#number NaN +10: %d" %(10+len(filter(lambda x: 'total_payments' in x[1] and x[1]['total_payments']=='NaN', enron_data.items())))
+print "#number POIs +10: %d" %(10+ len(filter(lambda (k,v): v["poi"], enron_data.items())))
+
+print "%%!poi stock_value: %f" %(len(filter(lambda x: 'stock_value' in x[1] and x[1]['stock_value']=='NaN' and not x[1]['poi'], enron_data.items()))/float(len(filter(lambda (k,v): not v["poi"], enron_data.items()))))
+print "%%poi stock_value: %f" %(len(filter(lambda x: 'stock_value' in x[1] and x[1]['stock_value']=='NaN' and x[1]['poi'], enron_data.items()))/float(len(filter(lambda (k,v): v["poi"], enron_data.items()))))
